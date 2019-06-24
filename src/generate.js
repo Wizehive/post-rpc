6de53bb2c8c6203c3f745bae5e900be31fb43173
Dave McNelis <davemcnelis@gmail.com>
2019-06-24 16:32:29 -0400

    Add serice validation

--- src/generate.js
+++ src/generate.js
@@ -5,6 +5,129 @@ const program = require('commander')
 const chalk = require('chalk')
 const yaml = require('js-yaml')
 const hogan = require('hogan.js')
+var Ajv = require('ajv')
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Schema
+//
+
+// Definitions
+//
+var defsSchema = {
+	$schema: 'http://json-schema.org/draft-07/schema#',
+	$id: 'http://localhost/schemas/defs.json#',
+	title: 'Definitions',
+	definitions: {
+		type: {
+			$id: '#type',
+			enum: [
+				'Boolean',
+				'Null',
+				'Undefined',
+				'Number',
+				'String',
+				'Object',
+				'Array'
+			]
+		}
+	}
+}
+
+//Argument
+var argumentSchema = {
+	$schema: 'http://json-schema.org/draft-07/schema#',
+	$id: 'http://localhost/schemas/argument.json#',
+	title: 'Argument',
+	type: 'object',
+	required: [
+		'arg',
+		'type'
+	],
+	properties: {
+		arg: {
+			type: 'string'
+		},
+		type: {
+			$ref: 'defs.json#type'
+		}
+	}
+}
+
+// Function
+var functionSchema = {
+	$schema: 'http://json-schema.org/draft-07/schema#',
+	$id: 'http://localhost/schemas/function.json#',
+	title: 'Function',
+	type: 'object',
+	required: [
+		'function',
+		'args',
+		'return',
+		'description'
+	],
+	properties: {
+		function: {
+			type: 'string'
+		},
+		args: {
+			type: [
+				'null',
+				'array'
+			],
+			items: { $ref: 'argument.json#' }
+		},
+		return: {
+			$ref: 'defs.json#type'
+		},
+		description: {
+			type: 'string'
+		}
+	}
+}
+
+// Service
+var serviceSchema = {
+	$schema: 'http://json-schema.org/draft-07/schema#',
+	$id: 'http://localhost/schemas/service.json#',
+	title: 'Service',
+	type: 'object',
+	required: [
+		'description',
+		'object',
+		'functions'
+	],
+	properties: {
+		description: {
+			type: 'string'
+		},
+		object: {
+			type: [
+				'null',
+				'string'
+			]
+		},
+		function: {
+			type: 'string'
+		},
+		functions: {
+			type: 'array',
+			items: { $ref: 'function.json#' }
+		},
+		return: {
+			$ref: 'defs.json#type'
+		}
+	}
+}
+
+// Services
+var servicesSchema = {
+	$schema: 'http://json-schema.org/draft-07/schema#',
+	$id: 'http://localhost/schemas/services.json#',
+	title: 'A collection of Post-RPC services',
+	type: 'array',
+	items: {$ref: 'service.json#'}
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -13,14 +136,14 @@ const hogan = require('hogan.js')
 
 const templates = {
 	serverProlouge: hogan.compile(`{{#service.description}}  // {{service.description}}
-  //
+	//
 {{/service.description}}`),
 	server: hogan.compile(`  {{server}}.register('{{#service.object}}{{service.object}}.{{/service.object}}{{def.function}}', [{{#def.args}}['{{arg}}', '{{type}}']{{^last}}, {{/last}}{{/def.args}}], '{{def.return}}', {{#service.object}}{{service.object}}.{{/service.object}}{{def.function}}, '{{def.description}}');
 `),
 	serverEpilouge: hogan.compile(`
 `),
 	clientProlouge: hogan.compile(`{{#service.description}}  // {{service.description}}
-  //
+	//
 {{/service.description}}{{#service.object}}  var {{service.object}} = {};
 {{/service.object}}`),
 	client: hogan.compile(`  {{^service.object}}var {{/service.object}}{{#service.object}}{{service.object}}.{{/service.object}}{{def.function}} = function({{#def.args}}{{arg}}{{^last}}, {{/last}}{{/def.args}}) {
@@ -42,6 +165,28 @@ var debug = function(message) {
 	console.error(chalk.magenta(message))
 }
 
+var load = function(serviceFile) {
+	try {
+		definitions = yaml.safeLoad(fs.readFileSync(serviceFile, 'utf8'))
+		var ajv = new Ajv({verbose: true})
+			.addSchema(defsSchema, 'defs')
+			.addSchema(argumentSchema, 'argument')
+			.addSchema(functionSchema, 'function')
+			.addSchema(serviceSchema, 'service')
+			.addSchema(servicesSchema, 'services')
+		var valid = ajv.validate('services', definitions)
+		if (!valid) {
+			console.error(chalk.red('error: Invalid Service'))
+			console.error(chalk.red(ajv.errorsText(ajv.errors, {dataVar: serviceFile, separator: '\n'})))
+			process.exit(1)
+		}
+
+	} catch (error) {
+		die(error)
+	}
+	return definitions
+}
+
 var render = function(data, template, stream) {
 	try {
 		stream.write(template.render(data));
@@ -60,22 +205,12 @@ var generate = function(serviceFile, type, {
 		serviceFile = serviceFile + '.yml'
 	}
 
-	//debug('serviceFile: ' + serviceFile)
 	output = serviceFile.replace(/\.yaml|\.yml/i,'.'+type+'.js')
 	if (stdout) {
 		output = '-'
 	}
-	//debug('client: ' + client)
-	//debug('server: ' + server)
-	//debug('output: ' + output)
 
-	var definitions
-	try {
-		definitions = yaml.safeLoad(fs.readFileSync(serviceFile, 'utf8'))
-	} catch (error) {
-		die(error)
-	}
-	//debug(serviceFile + ': ' + JSON.stringify(definitions, null, 2))
+	var definitions = load(serviceFile)
 
 	var wstream
 	try {
