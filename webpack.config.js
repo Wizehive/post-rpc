6645efedeab6d1c5f79328096408502d3cd95bb7
tehpsalmist <bstewardcodes@gmail.com>
2019-08-06 18:50:47 -0400

    refactor: freshen up webpack, babel, and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,64 @@
 /* global __dirname, require, module*/
 
 const webpack = require('webpack')
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
 const path = require('path')
 const env = require('yargs').argv.env // use --env with webpack 2
 
-var libraryName = 'PostRPC'
+const libraryName = 'PostRPC'
 
 let suffix
-let plugins = []
 
-module.exports = function(env) {
-	env = env || 'dev'
-	if (env === 'prod') {
-	  plugins.push(new UglifyJsPlugin({ minimize: true }))
-	  suffix = '.min.js'
-	} else {
-	  suffix = '.js'
-	}
+module.exports = env => {
+  env = env || 'dev'
 
-  return {
-		entry: {
-			Client: __dirname + "/src/client.js",
-			Server: __dirname + "/src/server.js"
-		},
-	  devtool: 'source-map',
-	  output: {
-			path: path.join(__dirname, "/lib"),
-			filename: libraryName + ".[name]"+suffix,
-			library: [libraryName, "[name]"],
-			libraryTarget: 'umd',
-			umdNamedDefine: true
-	  },
-	  module: {
-	    rules: [
-	      {
-	        test: /(\.jsx|\.js)$/,
-	        loader: 'babel-loader',
-	        exclude: /(node_modules|bower_components)/
-	      },
-	      {
-	        test: /(\.jsx|\.js)$/,
-	        loader: 'eslint-loader',
-	        exclude: /node_modules/
-	      }
-	    ]
-	  },
-	  resolve: {
-	    modules: [path.resolve('./node_modules'), path.resolve('./src')],
-	    extensions: ['.json', '.js']
-	  },
-		plugins: plugins
-	}
+  if (env === 'prod') {
+    suffix = '.min.js'
+  } else {
+    suffix = '.js'
+  }
+
+  return ['client', 'server'].map(name => ({
+    mode: env === 'prod' ? 'production' : 'development',
+    entry: `${__dirname}/packages/${name}/src/${name}.js`,
+    devtool: env === 'prod' && 'source-map',
+    optimization: {
+      minimize: env === 'prod'
+    },
+    output: {
+      path: path.join(__dirname, 'packages', name, 'dist'),
+      filename: `${libraryName}.${capitalize(name)}${suffix}`,
+      library: capitalize(name),
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    module: {
+      rules: [
+        {
+          test: /(\.jsx|\.js)$/,
+          loader: 'babel-loader',
+          exclude: /(node_modules|bower_components)/
+        },
+        {
+          test: /(\.jsx|\.js)$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      modules: [path.resolve('./node_modules'), path.resolve('./src')],
+      extensions: ['.json', '.js']
+    },
+    plugins: []
+  }))
+}
+
+/**
+ * 
+ * @param {string} string
+ * 
+ * @returns {string} capitalized string
+ */
+function capitalize (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
